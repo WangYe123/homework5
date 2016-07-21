91e42210d6b70a10c64afc9cb237f7e8fa81efbd
WangYe123 <bupt_wangye@163.com>
2016-07-21 12:29:27 +0800

    第五次作业提交

---
+++ CoinChange.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int [] dp = new int [amount + 1];
+        dp[0] = 0;
+        for (int i = 1; i < dp.length; i++){
+            dp[i] = Integer.MAX_VALUE;
+        }
+        for (int i = 1; i<=amount; i++){
+            for (int j = 0; j < coins.length; j++){
+                if (coins[j] <= i){
+                    if (dp[i - coins[j]] != Integer.MAX_VALUE){
+                        dp[i] = Math.min (dp[i], dp[i - coins[j]] + 1);
+                    }
+                }
+            }
+        }
+        return dp[amount] == Integer.MAX_VALUE ? -1 : dp[amount];
+    }
+}
